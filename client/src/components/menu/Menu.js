7ece27be42444fbb5e71c70586574de433067376
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-06-27 23:02:14 -0700

    Project setup

---
+++ client/src/components/menu/Menu.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#"></a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                About me <span class="sr-only"></span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                My work
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Portfolio <span class="sr-only"></span>
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Contact me<span class="sr-only"></span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Menu;
